ed53f4148fd7a7521a3509e20f359579b76bc731
marcocastignoli <marco.castignoli@gmail.com>
2019-03-31 22:54:14 +0200

    Fix migration

--- migrations/2019-02-15-095600_reactions/up.sql
+++ migrations/2019-02-15-095600_reactions/up.sql
@@ -3,7 +3,7 @@ CREATE TABLE reactions (
     reaction_type int(3) NOT NULL,
     author_id int(11) NOT NULL,
     post_id int(11) NOT NULL,
-    created DATETIME DEFAULT CURRENT_TIMESTAMP
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (reaction_id),
     foreign key (post_id) references posts(post_id),
     foreign key (author_id) references users(user_id)
